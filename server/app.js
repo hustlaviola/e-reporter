d2d98d9694224bf8282fbc86f6a573666ecce490
Viola <vvvviola@yahoo.com>
2019-01-12 08:29:02 +0100

    added url encoding

--- server/app.js
+++ server/app.js
@@ -4,6 +4,7 @@ import router from './routes/router';
 const app = express();
 
 app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
 app.get('/', (req, res) => {
   res.status(200).send('Welcome to iReporter');
